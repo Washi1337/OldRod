cd26ed12880edb3c1b2dc0db09d066616e60e317
Washi <washiwashi1337@gmail.com>
2019-03-09 23:10:14 +0100

    Updated file headers.

--- src/OldRod.Pipeline/Devirtualiser.cs
+++ src/OldRod.Pipeline/Devirtualiser.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// Project OldRod - A KoiVM devirtualisation utility.
+// Copyright (C) 2019 Washi
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using AsmResolver;
@@ -14,13 +30,13 @@ using OldRod.Pipeline.Stages.KoiStreamParsing;
 using OldRod.Pipeline.Stages.OpCodeResolution;
 using OldRod.Pipeline.Stages.Recompiling;
 using OldRod.Pipeline.Stages.VMCodeRecovery;
-using OldRod.Pipeline.Stages.VMEntryDetection;
+using OldRod.Pipeline.Stages.VMMethodDetection;
 
 namespace OldRod.Pipeline
 {
     public class Devirtualiser
     {
-        public const string Tag = "Main";
+        private const string Tag = "Main";
 
         public Devirtualiser(ILogger logger)
         {
