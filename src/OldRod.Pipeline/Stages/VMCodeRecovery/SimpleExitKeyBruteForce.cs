ae116448634a93300fccaab6b4466c50d35a3b06
Washi <washi1337@protonmail.com>
2020-05-17 13:24:33 +0200

    Initial port simple exit key bruteforce to v4.

--- src/OldRod.Pipeline/Stages/VMCodeRecovery/SimpleExitKeyBruteForce.cs
+++ src/OldRod.Pipeline/Stages/VMCodeRecovery/SimpleExitKeyBruteForce.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
+using AsmResolver;
 using OldRod.Core;
 using OldRod.Core.Architecture;
 using OldRod.Core.Disassembly.Inference;
@@ -32,20 +33,23 @@ namespace OldRod.Pipeline.Stages.VMCodeRecovery
                 logger.Warning(Tag, $"Cannot brute-force the exit key of function_{function.EntrypointAddress:X4} as it has no recorded call references.");
                 return null;
             }
-            
-            var data = koiStream.Data;
+
+            var reader = koiStream.Contents.CreateReader();
             
             // Find any call reference.
             foreach (var callReference in callReferences)
             {
                 var call = callReference.Caller.Instructions[callReference.Offset];
                 long targetOffset = call.Offset + call.Size;
+                reader.FileOffset = (uint) targetOffset;
+                byte[] encryptedOpCodes = new byte[3];
+                reader.ReadBytes(encryptedOpCodes, 0, encryptedOpCodes.Length);
 
                 // Go over all possible LSBs.
                 for (uint lsb = 0; lsb < byte.MaxValue; lsb++)
                 {
                     // Check whether the LSB decodes to a PUSHR_xxxx.
-                    if (IsPotentialLSB(constants, data[targetOffset], lsb))
+                    if (IsPotentialLSB(constants, encryptedOpCodes[0], lsb))
                     {
                         // Go over all remaining 24 bits.
                         for (uint i = 0; i < 0x00FFFFFF; i++)
@@ -53,7 +57,7 @@ namespace OldRod.Pipeline.Stages.VMCodeRecovery
                             uint currentKey = (i << 8) | lsb;
                             
                             // Try new key.
-                            if (IsValidKey(constants, data, targetOffset, currentKey))
+                            if (IsValidKey(constants, encryptedOpCodes, currentKey))
                             {
                                 // We have found a key!
                                 return currentKey;
@@ -86,10 +90,10 @@ namespace OldRod.Pipeline.Stages.VMCodeRecovery
             }
         }
 
-        private static bool IsValidKey(VMConstants constants, byte[] data, long offset, uint key)
+        private static bool IsValidKey(VMConstants constants, byte[] data, uint key)
         {
             // Opcode byte.
-            byte pushRDword = DecryptByte(data[offset], ref key);
+            byte pushRDword = DecryptByte(data[0], ref key);
             if (!constants.OpCodes.TryGetValue(pushRDword, out var opCode))
                 return false;
 
@@ -106,10 +110,10 @@ namespace OldRod.Pipeline.Stages.VMCodeRecovery
             }
 
             // Fixup byte.
-            byte fixup = DecryptByte(data[offset + 1], ref key);
+            byte fixup = DecryptByte(data[1], ref key);
 
             // Register operand.
-            byte rawRegister = DecryptByte(data[offset + 2], ref key);
+            byte rawRegister = DecryptByte(data[2], ref key);
             if (!constants.Registers.TryGetValue(rawRegister, out var register))
                 return false;
             
